cf61f3a713fbb73349395ba634d407a7ef274d39
vikyrg05 <vikyrg05@gmail.com>
2026-09-16 19:42:58 -0400

    Complete W03 route and query parameters

--- src/models/organizations.js
+++ src/models/organizations.js
@@ -1,14 +1,34 @@
 import db from './db.js'
 
 const getAllOrganizations = async () => {
-    const query = `
+  const query = `
         SELECT organization_id, name, description, contact_email, logo_filename
       FROM public.organization;
     `;
 
-    const result = await db.query(query);
+  const result = await db.query(query);
 
-    return result.rows;
+  return result.rows;
 }
 
-export { getAllOrganizations }  
+const getOrganizationDetails = async (organizationId) => {
+  const query = `
+    SELECT
+        organization_id,
+        name,
+        description,
+        contact_email,
+        logo_filename
+    FROM organization
+    WHERE organization_id = $1;
+    `;
+
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
+
+  // Return the first row of the result set, or null if no rows are found
+  return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+// Export the model functions
+export { getAllOrganizations, getOrganizationDetails };
